34726d9b965f992caeffb73442e815af94271f24
shaid30044 <kaona30044@gmail.com>
2023-12-19 22:02:47 +0600

    add favicon and lobo in Navbar

--- src/Shared/Home/Navbar/Navbar.jsx
+++ src/Shared/Home/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import "./navbar.css";
+import logo from "../../../assets/logos/logo.png";
 
 const Navbar = () => {
   const pages = [
@@ -23,6 +24,15 @@ const Navbar = () => {
 
   return (
     <div className="bg-white grid grid-cols-2 sm:grid-cols-4 items-center px-4 md:px-10 lg:px-20 py-2 md:py-4">
+      {/* logo */}
+
+      <div className="w-40">
+        <Link to="/" className="flex items-center gap-2">
+          <img src={logo} alt="logo" className="h-10" />
+          <p className="text-2xl font-semibold text-primary">Mobile Island</p>
+        </Link>
+      </div>
+
       {/* for medium & large device */}
 
       <div className="hidden col-span-2 sm:flex justify-center">
