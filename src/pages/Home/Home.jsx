fe6ebf9b30f28ac6de2b99326f9fefdcada3a57f
shaid30044 <kaona30044@gmail.com>
2023-12-19 22:21:08 +0600

    install react helmet async

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,13 @@
+import { Helmet } from "react-helmet-async";
 import Navbar from "../../Shared/Home/Navbar/Navbar";
 
 const Home = () => {
   return (
     <div>
+      <Helmet>
+        <title>Mobile Island</title>
+      </Helmet>
+
       <Navbar />
     </div>
   );
